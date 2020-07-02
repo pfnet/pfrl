26d6a31ac0cff5b0906e4e150914b837c874b306
muupan <muupan@gmail.com>
2020-07-02 21:49:57 +0900

    Add test cases with non-scalar event shape

--- tests/utils_tests/test_sample_with_log_prob.py
+++ tests/utils_tests/test_sample_with_log_prob.py
@@ -35,6 +35,36 @@ def test_transform_mv_normal():
         torch.allclose(log_p, log_prob)
 
 
+def test_transform_independent_normal():
+    # batch size is 3
+    loc = torch.as_tensor([[0.3, 0.7], [0.2, 0.4], [0.8, 0.9]])
+    scale = torch.as_tensor([[0.1, 0.9], [0.2, 0.8], [0.3, 0.7]])
+    base_dist = distributions.Independent(distributions.Normal(loc, scale), 1)
+    distrib = distributions.TransformedDistribution(
+        base_dist, [distributions.transforms.TanhTransform()]
+    )
+    for _ in range(10):
+        y, log_prob = sample_with_log_prob(distrib)
+        log_p = distrib.log_prob(y)
+        torch.allclose(log_p, log_prob)
+
+
+def test_transform_batch_mv_normal():
+    # batch size is 3
+    loc = torch.as_tensor([[0.3, 0.7], [0.2, 0.4], [0.8, 0.9]])
+    cov = torch.as_tensor(
+        [[[0.1, 0.0], [0.0, 0.9]], [[0.5, 0.0], [0.0, 0.6]], [[0.7, 0.0], [0.0, 0.1]]]
+    )
+    base_dist = distributions.MultivariateNormal(loc, cov)
+    distrib = distributions.TransformedDistribution(
+        base_dist, [distributions.transforms.TanhTransform()]
+    )
+    for _ in range(10):
+        y, log_prob = sample_with_log_prob(distrib)
+        log_p = distrib.log_prob(y)
+        torch.allclose(log_p, log_prob)
+
+
 def test_multivariate_normal():
     loc = torch.as_tensor([0.3, 0.7])
     cov = torch.as_tensor([[0.1, 0.0], [0.0, 0.9]])
