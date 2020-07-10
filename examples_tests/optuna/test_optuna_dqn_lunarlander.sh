bf840d6bfd528a6e309ba6ebf6963711ca6d3e29
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2020-07-10 12:41:47 +0900

    add missing hyphen

--- examples_tests/optuna/test_optuna_dqn_lunarlander.sh
+++ examples_tests/optuna/test_optuna_dqn_lunarlander.sh
@@ -12,5 +12,5 @@ study="optuna-pfrl-test"
 optuna create-study --study-name "${study}" --storage "${storage}" --direction maximize
 
 python examples/optuna/optuna_dqn_lunarlander.py \
-  -optuna-study-name "${study}" --optuna-storage "${storage}" --optuna-n-trials 3 \
+  --optuna-study-name "${study}" --optuna-storage "${storage}" --optuna-n-trials 3 \
   --steps 100 --gpu $gpu --outdir $outdir/optuna/dqn_lunarlander
