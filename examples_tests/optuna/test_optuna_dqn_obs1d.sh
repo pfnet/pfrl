29e8c4293577844bfd86593647fdd1f60914c27c
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2020-07-10 15:55:35 +0900

    fix wrong script path

--- examples_tests/optuna/test_optuna_dqn_obs1d.sh
+++ examples_tests/optuna/test_optuna_dqn_obs1d.sh
@@ -6,12 +6,12 @@ outdir=$(mktemp -d)
 
 gpu="$1"
 
-# optuna/dqn_lunarlander
+# optuna/dqn_obs1d
 storage="sqlite:///${outdir}/tmp.db"
 study="optuna-pfrl-test"
 optuna create-study --study-name "${study}" --storage "${storage}" --direction maximize
 
-python examples/optuna/optuna_dqn_lunarlander.py \
+python examples/optuna/optuna_dqn_obs1d.py \
   --env "CartPole-v0" \
   --optuna-study-name "${study}" --optuna-storage "${storage}" --optuna-n-trials 3 \
-  --steps 100 --gpu $gpu --outdir $outdir/optuna/dqn_lunarlander
+  --steps 100 --gpu $gpu --outdir $outdir/optuna/dqn_obs1d
