6da30451fc640169da350ec77e2e3577e00bf673
Tianqi Xu <tianqi@preferred.jp>
2020-07-15 09:07:14 +0000

    add update_counter
    This PR changes the type of `update_counter` from a pure int to a
    `multiprocessing.value`. The reason behind this change is
    that the `update_counter` is used as the
    `generation id` in the `grpc` mode and is shared with the server process.
    
    A lock is added to secure that no broken model will be shared between
    the server and learner in grpc mode.

--- examples/gym/train_dqn_gym.py
+++ examples/gym/train_dqn_gym.py
@@ -19,6 +19,8 @@ import torch.optim as optim
 import gym
 from gym import spaces
 import numpy as np
+import ctypes
+import multiprocessing as mp
 
 import pfrl
 from pfrl.agents.dqn import DQN
@@ -238,12 +240,13 @@ def main():
         # Disable the multi-threading on Openmp and Numpy.
         os.environ["OMP_NUM_THREADS"] = "1"  # NOQA
 
+        update_counter = mp.Value(ctypes.c_ulong)
         (
             make_actor,
             learner,
             poller,
             exception_event,
-        ) = agent.setup_actor_learner_training(args.num_envs)
+        ) = agent.setup_actor_learner_training(args.num_envs, update_counter)
 
         poller.start()
         learner.start()
