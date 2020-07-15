d54f4800a9c0b4d7f1403906af61cd41a2720bd8
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2020-07-15 18:39:05 +0900

    add test for train_agent with evaluation_hooks

--- tests/experiments_tests/test_train_agent.py
+++ tests/experiments_tests/test_train_agent.py
@@ -108,3 +108,75 @@ class TestTrainAgent(unittest.TestCase):
             self.assertEqual(args[1], agent)
             # step starts with 1
             self.assertEqual(args[2], i + 1)
+
+    def test_with_evaluation_hooks(self):
+
+        outdir = tempfile.mkdtemp()
+
+        agent = mock.Mock()
+        env = mock.Mock()
+        # Reaches the terminal state after five actions
+        env.reset.side_effect = [("state", 0)]
+        env.step.side_effect = [
+            (("state", 1), 0, False, {}),
+            (("state", 2), 0, False, {}),
+            (("state", 3), -0.5, False, {}),
+            (("state", 4), 0, False, {}),
+            (("state", 5), 1, True, {}),
+        ]
+        hook = mock.Mock()
+
+        n_resets = 1  # Statistics will be collected when episodes end.
+        dummy_stats = [
+            ("average_q", 3.14),
+            ("average_loss", 2.7),
+            ("cumulative_steps", 42),
+            ("n_updates", 8),
+            ("rlen", 1),
+        ]
+        agent.get_statistics.side_effect = [dummy_stats] * n_resets
+
+        evaluator = mock.Mock()
+        evaluator.evaluate_if_necessary.return_value = 42
+
+        evaluation_hook = mock.Mock()
+
+        statistics = pfrl.experiments.train_agent(
+            agent=agent,
+            env=env,
+            steps=5,
+            outdir=outdir,
+            step_hooks=[hook],
+            evaluator=evaluator,
+            evaluation_hooks=[evaluation_hook],
+        )
+
+        expected_statistics = [dict(dummy_stats) for _ in range(n_resets)]
+        expected_statistics[-1]["eval_score"] = 42
+        self.assertListEqual(statistics, expected_statistics)
+
+        self.assertEqual(agent.act.call_count, 5)
+        self.assertEqual(agent.observe.call_count, 5)
+        # done=True at state 5
+        self.assertTrue(agent.observe.call_args_list[4][0][2])
+
+        self.assertEqual(env.reset.call_count, 1)
+        self.assertEqual(env.step.call_count, 5)
+
+        self.assertEqual(hook.call_count, 5)
+        # A hook receives (env, agent, step)
+        for i, call in enumerate(hook.call_args_list):
+            args, kwargs = call
+            self.assertEqual(args[0], env)
+            self.assertEqual(args[1], agent)
+            # step starts with 1
+            self.assertEqual(args[2], i + 1)
+
+        # evaluation_hook receives (env, agent, evaluator, t, eval_score)
+        self.assertEqual(evaluation_hook.call_count, 1)
+        args = evaluation_hook.call_args[0]
+        self.assertIs(args[0], env)
+        self.assertIs(args[1], agent)
+        self.assertIs(args[2], evaluator)
+        self.assertIs(args[3], 5)
+        self.assertIs(args[4], 42)
