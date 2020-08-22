e24e48f948ad1786ec54b9bed88183db337a2223
Matthew Strong <mast4878@colorado.edu>
2020-08-21 23:45:19 -0600

    fix

--- pfrl/agents/hrl/utils/hiro_utils.py
+++ pfrl/agents/hrl/utils/hiro_utils.py
@@ -42,63 +42,6 @@ def _is_update(episode, freq, ignore=0, rem=0):
     return False
 
 
-class ReplayBuffer():
-    def __init__(self, state_dim, goal_dim, action_dim, buffer_size, batch_size):
-        self.buffer_size = buffer_size
-        self.batch_size = batch_size
-        self.ptr = 0
-        self.size = 0
-        self.state = np.zeros((buffer_size, state_dim))
-        self.goal = np.zeros((buffer_size, goal_dim))
-        self.action = np.zeros((buffer_size, action_dim))
-        self.n_state = np.zeros((buffer_size, state_dim))
-        self.reward = np.zeros((buffer_size, 1))
-        self.not_done = np.zeros((buffer_size, 1))
-
-        self.device = device
-
-    def append(self, state, goal, action, n_state, reward, done):
-        self.state[self.ptr] = state
-        self.goal[self.ptr] = goal
-        self.action[self.ptr] = action
-        self.n_state[self.ptr] = n_state
-        self.reward[self.ptr] = reward
-        self.not_done[self.ptr] = 1. - done
-
-        self.ptr = (self.ptr+1) % self.buffer_size
-        self.size = min(self.size+1, self.buffer_size)
-
-    def sample(self):
-        ind = np.random.randint(0, self.size, size=self.batch_size)
-
-        return (
-            torch.FloatTensor(self.state[ind]).to(self.device),
-            torch.FloatTensor(self.goal[ind]).to(self.device),
-            torch.FloatTensor(self.action[ind]).to(self.device),
-            torch.FloatTensor(self.n_state[ind]).to(self.device),
-            torch.FloatTensor(self.reward[ind]).to(self.device),
-            torch.FloatTensor(self.not_done[ind]).to(self.device),
-        )
-
-
-class SubgoalActionSpace(object):
-    def __init__(self, dim):
-        limits = np.array([-10, -10, -0.5, -1, -1, -1, -1,
-                           -0.5, -0.3, -0.5, -0.3, -0.5, -0.3, -0.5, -0.3])
-        self.shape = (dim, 1)
-        self.low = limits[:dim]
-        self.high = -self.low
-
-    def sample(self):
-        return (self.high - self.low) * np.random.sample() + self.low
-
-
-class Subgoal(object):
-    def __init__(self, dim=15):
-        self.action_space = SubgoalActionSpace(dim)
-        self.action_dim = self.action_space.shape[0]
-
-
 def record_experience_to_csv(args, experiment_name, csv_name='experiments.csv'):
     # append DATE_TIME to dict
     d = vars(args)
