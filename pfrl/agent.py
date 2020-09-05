5a38c9e6e392c7e3f1400305c247f94d59f0652c
Matthew Strong <mast4878@colorado.edu>
2020-09-05 11:19:55 -0600

    delete unused imports

--- pfrl/agent.py
+++ pfrl/agent.py
@@ -8,7 +8,6 @@ from typing import List
 from typing import Optional
 from typing import Sequence
 from typing import Tuple
-from pfrl.utils import subgoal
 
 import torch
 import numpy as np
--- pfrl/experiments/train_hrl_agent.py
+++ pfrl/experiments/train_hrl_agent.py
@@ -11,7 +11,6 @@ import argparse
 import numpy as np
 import datetime
 import copy
-from pfrl.utils import subgoal
 
 from torch import sub
 from gym import spaces
