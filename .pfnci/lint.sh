4ada630f9f19c59aefd064333fd09429ce7cbbbc
muupan <muupan@gmail.com>
2021-01-27 20:15:18 +0900

    Switch to mypy>=0.800

--- .pfnci/lint.sh
+++ .pfnci/lint.sh
@@ -2,13 +2,11 @@
 
 set -eux
 
-pip3 install black flake8 mypy isort
+pip3 install black flake8 'mypy>=0.800' isort
 
 black --diff --check pfrl tests examples
 isort --diff --check pfrl tests examples
 flake8 pfrl tests examples
-mypy pfrl
-# mypy does not search child directories unless there is __init__.py
-find tests -type f -name "*.py" | xargs dirname | sort | uniq | xargs mypy
+mypy pfrl tests
 # To avoid "duplicate module named xxx.py" errors, run mypy independently for each directory
 find examples -type f -name "*.py" | xargs dirname | sort | uniq | xargs -I{} mypy {}
