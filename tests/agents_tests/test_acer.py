711bff6b7943974c9ce6c34955ef93291133eb44
muupan <muupan@gmail.com>
2021-05-26 23:57:07 +0900

    Support fixed covariance by filtering out non-learnable params
    Loss function used in the test is changed because maximizing the entropy has
    no effect for Gaussian with fixed covariance

--- tests/agents_tests/test_acer.py
+++ tests/agents_tests/test_acer.py
@@ -295,11 +295,13 @@ def _test_compute_loss_with_kl_constraint(base_policy):
     with torch.no_grad():
         # Compute KL divergence against the original distribution
         base_distrib = base_policy(x)
+        some_action = base_distrib.sample()
 
     def base_loss_func(distrib):
-        # Any loss that tends to increase KL divergence should be ok
-        kl = torch.distributions.kl_divergence(base_distrib, distrib)
-        return -(kl + distrib.entropy())
+        # Any loss that tends to increase KL divergence should be ok.
+        # Here I choose to minimize the log probability of some fixed action.
+        # The loss is clipped to avoid NaN.
+        return torch.max(distrib.log_prob(some_action), torch.as_tensor(-20.))
 
     def compute_kl_after_update(loss_func, n=100):
         policy = copy.deepcopy(base_policy)
