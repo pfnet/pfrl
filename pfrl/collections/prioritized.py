a733679b0f1aef42631fdb337890e3ab9b65c49f
muupan <muupan@gmail.com>
2022-03-13 16:02:40 +0900

    Fix mypy error due to numbers.Number

--- pfrl/collections/prioritized.py
+++ pfrl/collections/prioritized.py
@@ -1,5 +1,4 @@
 import collections
-from numbers import Number
 from typing import (
     Any,
     Callable,
@@ -243,7 +242,7 @@ class TreeQueue(Generic[V]):
         return ret
 
 
-def _find(index_left: int, index_right: int, node: Node, pos: Number) -> int:
+def _find(index_left: int, index_right: int, node: Node, pos: float) -> int:
     if index_right - index_left == 1:
         return index_left
     else:
